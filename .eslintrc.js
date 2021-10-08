54ba386c8db4682f5b37cf27c2bb9074c8fddb83
osbornetunde <osbornetunde@gmail.com>
2021-10-09 00:07:45 +0100

    project-setup

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/warnings',
+    'plugin:import/errors',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'prettier',
+    'import',
+    'jsx-a11y',
+    '@typescript-eslint',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'jest.setup.ts',
+          '**/*.test.tsx',
+          '**/.spec.tsx',
+          '**/.test.ts',
+          '**/.spec.ts',
+        ],
+      },
+    ],
+  },
+};
